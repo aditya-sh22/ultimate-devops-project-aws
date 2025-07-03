8dadcd70152f9949d2ff615901e15943c3d71f31
aditya-sh22 <aditya03092@outlook.com>
2025-07-03 19:51:34 +0530

    Updated S3 bucket name

--- eks-install/main.tf
+++ eks-install/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "demo-terraform-eks-state-s3-bucket"
+    bucket         = "terraform-eks-state-s3-bucket-1415"
     key            = "terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "terraform-eks-state-locks"
